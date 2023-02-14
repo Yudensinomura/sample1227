02e85c62e95e6470da6d778d47fe0cfd4788e52e
yuden <you@example.com>
2023-02-14 10:59:27 +0000

    second arry

---
+++ datatype54_2.js
@@ -0,0 +1,26 @@
+let fruits = ["appe","Orange","Plum"];
+
+console.log( "fruits[0] is",fruits[0] );
+
+fruits[2] = "Pear";
+
+fruits[3] = "Lemon";
+
+console.log( "fruits[2] is",fruits[2]);
+
+console.log( "fruits[3] is",fruits[3]);
+
+console.log( "length is",fruits.length );
+
+console.log( "fruits array is",fruits );
+
+let mixArr = ['Appele', { name:'john'}, true, function() { console.log('Hellow');}];
+
+console.log( mixArr[1].name);
+console.log( mixArr[3]());
+
+ let fruitsObject = {
+  "0": "Apple",
+  "1": "Orange",
+  "2": "Plum",
+ }
