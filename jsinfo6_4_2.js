d942a5577b3a29abf302048e40ac40ecfc12a97c
yuden <you@example.com>
2023-04-25 10:03:59 +0000

    add TIME

---
+++ jsinfo6_4_2.js
@@ -0,0 +1,13 @@
+let message = "Hello";
+let message2 = 9;
+
+let obj = {
+    name:"John",
+    age:20
+};
+
+function sayHi(){
+    console.log("Hi");
+}
+
+console.log(sayHi.name);
---
+++ jsinfo6_8_1.js
@@ -0,0 +1,5 @@
+function sayHi(phrse,who){
+    console.log(phrse + ',' + who)
+};
+
+setTimeout(sayHi, 1000, "Hello", "JOHN");
