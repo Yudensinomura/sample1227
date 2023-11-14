11942ea37c4d5e89fda6d8ba250990388cce6d98
yuden <you@example.com>
2023-11-14 11:02:03 +0000

    こんにちは私は尊氏です

---
+++ statics/sample_function.js
@@ -0,0 +1,15 @@
+console.log("こんにちは");
+
+let userName = "尊氏"
+function showMessage(from,text){
+    
+    console.log(from);
+    console.log(text);
+}
+function sayHi(){
+    let message = "こんにちは私は"
+   console.log(message,userName)
+}
+showMessage("葵","こんにちは!");
+
+sayHi();
